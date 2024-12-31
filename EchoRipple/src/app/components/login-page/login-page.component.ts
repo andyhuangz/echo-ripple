3df0f93ead6537050855beb97c5d8b0b65fd6f84
Andy <andyhuang9338@gmail.com>
2024-12-30 21:56:22 -0800

    pages/login: added login page

---
+++ EchoRipple/src/app/components/login-page/login-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [NavBarComponent],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+
+}
